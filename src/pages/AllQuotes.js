895fd79b4889b605f5fd7aa2accda2d3aa619699
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-11-03 19:50:10 +0200

    quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+	{ id: 'q1', author: 'Mini', text: 'Learning React is cool!' },
+	{ id: 'q2', author: 'Jimnya', text: 'Learning React is fun!' }
+];
+
 const AllQuotes = () => {
-	return <h1>All quotes page</h1>;
+	return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,25 @@
 import { Route, useParams } from 'react-router';
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+	{ id: 'q1', author: 'Mini', text: 'Learning React is cool!' },
+	{ id: 'q2', author: 'Jimnya', text: 'Learning React is fun!' }
+];
 
 const QuoteDetail = () => {
 	const params = useParams();
 
+	const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+	if (!quote) {
+		return <p>No quote find!</p>;
+	}
+
 	return (
 		<>
-			<h1>Quote detail</h1>
-			<p>{params.quoteId}</p>
+			<HighlightedQuote text={quote.text} author={quote.author} />
 			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
