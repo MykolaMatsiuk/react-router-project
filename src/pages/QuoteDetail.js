afa26f3f9ec4cb77c1aeb68368502f1f00888bd9
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-11-03 18:21:18 +0200

    Route Nav Layout

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,19 @@
+import { Route, useParams } from 'react-router';
+
+import Comments from '../components/comments/Comments';
+
+const QuoteDetail = () => {
+	const params = useParams();
+
+	return (
+		<>
+			<h1>Quote detail</h1>
+			<p>{params.quoteId}</p>
+			<Route path={`/quotes/${params.quoteId}/comments`}>
+				<Comments />
+			</Route>
+		</>
+	);
+};
+
+export default QuoteDetail;
